480d01e83096d482e3d7a36a93b250990e3b8329
Ítalo Góes <italogoes85@gmail.com>
2023-06-24 22:37:40 -0300

    .

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
   res.send('Olá, mundo!');
 });
 
+app.get('/dados', (req, res) => {
+  const dados = {
+    nome: 'Italo',
+    idade: 23
+  }
+  res.send(dados)
+})
+
 // Defina a porta em que o servidor irá escutar
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
